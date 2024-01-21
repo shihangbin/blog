047ff5962e1ba55f3cd42470f4601120f6f8ec53
三九 <18213331550@163.com>
2024-01-21 17:27:01 +0800

    notes

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ git init
 git add -A
 git commit -m "更新文章"
 
-git remote add origin git@github.com:shihangbin/vitepress-notes.git
-# git remote add origin https://github.com/shihangbin/vitepress-notes.git
+# git remote add origin git@github.com:shihangbin/vitepress-notes.git
+git remote add origin https://github.com/shihangbin/vitepress-notes.git
 git push -f origin main:gh-pages 
 
 cd -
